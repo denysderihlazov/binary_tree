d7eeff7c03e4b4b3214524a68a106a2df838e206
denys <denysderihlazov@gmail.com>
2024-02-09 18:05:51 +0100

    Added: more information for HELP_INFO

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 #define HELP_INFO \
 " ---binary Tree--- \n"\
 "   Usage instructions:\n\n" \
-"\nAttention: \n\n" \
-"The <data> of structure must be an integer!\n"
+" - To add elements to the tree, run the program with integers as arguments.\n"\
+"   The first argument after the program name will be taken as the base key,\n"\
+"   and subsequent keys will be generated and added based on it.\n"\
+"   Example: ./main 10 will add keys around the base key 10.\n\n" \
+" - To search for an element, provide the key as the second argument.\n"\
+"   The program will search for this key in the tree and report if it was found.\n"\
+"   Example: ./main 10 15 will add keys around 10 and search for 15.\n"
 
-// AVL bin tree structure
+
 struct tree {
     int key;
     struct tree *left;
     struct tree *right;
-    int height;
 };
 
 void printHeloInfo()
@@ -21,19 +25,13 @@ void printHeloInfo()
     exit(0);
 }
 
-
-int strCompare(char *str1, char *str2)
-{
+int strCompare(char *str1, char *str2) {
     int i = 0;
-    while(*(str1+i) != '\0' && *(str2+i) != '\0') {
-        if((int)*(str1+i) == (int)*(str2+i) && (int)*(str1+i+1) == (int)*(str2+i+1)) {
-            i++;
-        } else {
-            return 0;
-        }
+    while(*(str1+i) == *(str2+i)) {
+        if(*(str1+i) == '\0') return 1;
+        i++;
     }
- 
-    return 1;
+    return 0;
 }
 
 int parseCharToInt(char *str)
@@ -69,46 +67,79 @@ int parseCharToInt(char *str)
 }
 
 void addNode(int key, struct tree **Root) {
-    // add Tree->key
-    // add Tree->left
-    // add Tree->right
     if(Root == NULL) {
         printf("Unable to allocate memory!\n");
         exit(1);
     }
 
     struct tree *newNode = malloc(sizeof(struct tree));
+    if (newNode == NULL) {
+        printf("Unable to allocate memory for new node!\n");
+        exit(2);
+    }
+
     newNode->key = key;
     newNode->left = NULL;
     newNode->right = NULL;
-    newNode->height = 1; // 1 means leave in AVL Tree
-    
 
-    if(*Root == NULL) { // The Tree is empty
+    if(*Root == NULL) { // The tree is empty
         *Root = newNode;
-    } else { // Add Node if the Tree isn't empty
-        //need to check elements in the Tree and check if there are any comparison
-
+    } else {
         struct tree *currentNode = *Root;
+        struct tree *parent = NULL;
 
-        
+        while(1) {
+            parent = currentNode;
 
-        if(newNode->key == currentNode->key) {
-            printf("Tree key already exists!\n");
-            // handle case!
-        } else if(newNode->key > currentNode->key) {
-            //move right
-            if(currentNode->right != NULL) {
-                currentNode = currentNode->right;
-            } 
-        } else if(newNode->key < currentNode->key){
-            //move left
-            if(currentNode->left != NULL) {
+            if(key == currentNode->key) {
+                printf("Tree key already exists!\n");
+                free(newNode);
+                return;
+            } else if(key < currentNode->key) {
                 currentNode = currentNode->left;
-            } 
+                if(currentNode == NULL) {
+                    parent->left = newNode;
+                    return;
+                }
+            } else {
+                currentNode = currentNode->right;
+                if(currentNode == NULL) {
+                    parent->right = newNode;
+                    return;
+                }
+            }
+        }
+    }
+}
+
+int searchThroughBinTree(int searchable, struct tree **Root) {
+    if(Root == NULL || *Root == NULL) {
+        printf("The tree is empty!\n");
+        return -1;
+    }
+
+    struct tree *currentNode = *Root;
+
+    while (currentNode != NULL) {
+        if(currentNode->key == searchable) {
+            printf("Found\n");
+            return 1;
+        } else if (searchable < currentNode->key) {
+            currentNode = currentNode->left;
+        } else if (searchable > currentNode->key) {
+            currentNode = currentNode->right;
         }
     }
 
+    printf("The element isn't in the tree\n");
+    return 0;
+}
+
+void freeTree(struct tree *node) {
+    if (node == NULL) return;
+    freeTree(node->left);
+    freeTree(node->right);
+    free(node);
 }
 
 int main(int argc, char *argv[])
@@ -117,30 +148,34 @@ int main(int argc, char *argv[])
         printHeloInfo();
     }
 
-    struct tree *Tree = malloc(sizeof(struct tree));
-    Tree->key = 0;
-    Tree->left = NULL;
-    Tree->right = NULL;
+    struct tree *Tree = NULL;
 
     if(strCompare(argv[1], "-h") || strCompare(argv[1], "--help")) {
         printHeloInfo();
     }
 
-    if(argc == 2) {
+    if(argc == 3) {
         int key = parseCharToInt(argv[1]);
         addNode(key, &Tree);
+        addNode(key+1, &Tree);
+        addNode(key+2, &Tree);
+        addNode(key+3, &Tree);
+        addNode(key+5, &Tree);
+        addNode(key+7, &Tree);
+        addNode(key+4, &Tree);
+        addNode(key+9, &Tree);
+
+        int searchKey = parseCharToInt(argv[2]);
+        printf("Searching for: %d\n", searchKey);
+        int found = searchThroughBinTree(searchKey, &Tree);
+        if(found == 1) {
+            printf("Element found.\n");
+        } else {
+            printf("Element not found.\n");
+        }
     }
 
-    // while(Tree != NULL) { // Finish memory free
-    //     struct tree *temp = Tree;
-    //     free(temp);
-    // }
+    freeTree(Tree);
 
     return 1;
 }
-
-// init and add elements to Tree
-// count Tree sum
-// find element in a Tree
-
-// in a future draw tree in a console
